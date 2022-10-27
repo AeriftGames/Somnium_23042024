f2f9d05a8b87fb32359c76103e472b2e5deba41c
Oalar <tomas.vresky@gmail.com>
2022-10-28 00:54:41 +0200

    another backup

--- item_inspect.gd
+++ item_inspect.gd
@@ -1,14 +1,23 @@
 extends Control
 
+var rotating: bool = false
+var prev_mouse_position
+var next_mouse_position
+
+var isNowInteract = true
+var passed_object
+var node_interact
+var player
+
 
 func _ready():
 	self.hide()
-	pass
 
 
 func _process(delta):
 	var texture = $SubViewport.get_texture()
 	$Sprite2D.texture = texture
+	_used(delta)
 
 
 func show_inspect(state):
@@ -16,3 +25,16 @@ func show_inspect(state):
 		self.show()
 	else:
 		self.hide()
+
+
+func _used(delta):
+	if (Input.is_action_just_pressed("Rotate")):
+		rotating = true
+		prev_mouse_position = get_viewport().get_mouse_position()
+	if (Input.is_action_just_released("Rotate")):
+		rotating = false
+	if rotating == true:
+		next_mouse_position = get_viewport().get_mouse_position()
+		$SubViewport.get_child(0).get_child(1).rotate_y((next_mouse_position.x - prev_mouse_position.x) * 1 * delta)
+		$SubViewport.get_child(0).get_child(1).rotate_z(-(next_mouse_position.y - prev_mouse_position.y) * 1 * delta)
+		prev_mouse_position = next_mouse_position
--- paper_test.gd
+++ paper_test.gd
@@ -16,7 +16,6 @@ func _ready():
 
 
 func _process(delta):
-	if isNowInteract == true:
 		_used(delta)
 
 	
@@ -31,28 +30,3 @@ func _used(delta):
 		rotate_y((next_mouse_position.x - prev_mouse_position.x) * 1 * delta)
 		rotate_z(-(next_mouse_position.y - prev_mouse_position.y) * 1 * delta)
 		prev_mouse_position = next_mouse_position
-
-
-func _used_quit():
-	player.show_inspect(false)
-	passed_object.SetInputEnable(true)
-	passed_object = null
-	isNowInteract = false
-
-
-func _input(event):
-	if event.is_action_pressed("Jump") and isNowInteract == true:
-		_used_quit()
-
-
-func message_update():
-	var msg:String = node_interact.msgObject.GetMessage()
-	if msg == "msg_get_use_action_text":
-		node_interact.msgObject.SetStringData("Rotate item")
-	if msg == "msg_get_interactive_object_name":
-		node_interact.msgObject.SetStringData("placeholder")
-	if msg == "msg_use_action":
-		passed_object = node_interact.msgObject.GetNodeData()
-		passed_object.SetInputEnable(false)
-		isNowInteract = true
-		player.show_inspect(true)
