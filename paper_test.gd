bc34cf41000af699161e51129f19afcb8fda7812
Oalar <tomas.vresky@gmail.com>
2022-10-27 21:15:40 +0200

    remove comments, add interact 2D hud to player

--- paper_test.gd
+++ paper_test.gd
@@ -40,9 +40,7 @@ func _used(delta):
 		rotating = false
 		
 	if rotating == true:
-		print("rotating")
 		next_mouse_position = get_viewport().get_mouse_position()
-		print(next_mouse_position)
 		rotate_y((next_mouse_position.x - prev_mouse_position.x) * 1 * delta)
 		rotate_z(-(next_mouse_position.y - prev_mouse_position.y) * 1 * delta)
 		prev_mouse_position = next_mouse_position
