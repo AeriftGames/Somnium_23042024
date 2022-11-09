7696a51e5294fb8a565d4608498fb43e60120c6a
Oalar <tomas.vresky@gmail.com>
2022-11-09 19:35:38 +0100

    add placeholder to item_use

--- core_systems/item_use/item_use.gd
+++ core_systems/item_use/item_use.gd
@@ -33,6 +33,8 @@ var sfx_node: Node
 
 
 func _ready() -> void:
+	if sfx != null:
+		sfx = load("res://objects/read/paper_test/page_flip.wav")
 	node_interact = $interactive_object
 	item_interaction = item_interaction_name + " " + item_name
 
