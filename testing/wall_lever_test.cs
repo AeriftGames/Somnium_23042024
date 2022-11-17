816f0bbfd9fd27f47a273f5e6859ee8744a5f32e
kaencz <kaencz@seznam.cz>
2022-11-17 23:10:28 +0100

    first lever prototype

--- testing/wall_lever_test.cs
+++ testing/wall_lever_test.cs
@@ -3,109 +3,172 @@ using System;
 
 public partial class wall_lever_test : Node3D
 {
-	public interactive_object interactiveObject = null;
-	public FPSCharacter_Interaction interactCharacter = null;
+    public interactive_object interactiveObject = null;
+    public FPSCharacter_Interaction interactCharacter = null;
 
-	RigidBody3D leverGrab = null;
-	HingeJoint3D hingeJoint3D = null;
+    RigidBody3D leverGrab = null;
+    HingeJoint3D hingeJoint3D = null;
 
-	bool isActionUpdate = false;
+    bool isActionUpdate = false;
 
-	public override void _Ready()
-	{
-		interactiveObject = GetNode<interactive_object>("LeverGrab/interactive_object");
+    //
+    Vector2 motionMouse = Vector2.Zero;
+    [Export] public float MotorPower = 3.0f;
+    float max = 60.0f;
+    float min = -60.0f;
+    bool isReach = false;
 
-		leverGrab = GetNode<RigidBody3D>("LeverGrab");
-		hingeJoint3D = GetNode<HingeJoint3D>("HingeJoint3D");
+    public override void _Ready()
+    {
+        interactiveObject = GetNode<interactive_object>("LeverGrab/interactive_object");
 
+        leverGrab = GetNode<RigidBody3D>("LeverGrab");
+        hingeJoint3D = GetNode<HingeJoint3D>("HingeJoint3D");
 
-	}
+        SetReachPositionFreeze(true);
+    }
     public override void _Input(InputEvent @event)
     {
         base._Input(@event);
-        
+
         if (@event is InputEventMouseMotion && isActionUpdate)
         {
             InputEventMouseMotion mouseEventMotion = @event as InputEventMouseMotion;
-            var a = mouseEventMotion.Relative;
-            
+            motionMouse = mouseEventMotion.Relative;
         }
     }
 
     public override void _Process(double delta)
-	{
-        GameMaster.GM.Log.WriteLog(this, LogSystem.ELogMsgType.INFO, "rotaceX= " + Mathf.RadToDeg(leverGrab.Rotation.x));
-
-        // odsud dolu je funkce pri grab action
-        if (!isActionUpdate) return;
-
-		if(Input.IsActionJustPressed("testTop"))
-		{
-			SetActualPositon(true);
-		}
-
-        if (Input.IsActionJustPressed("testBottom"))
+    {
+        // pokud jsme v GrabAction
+        if (isActionUpdate)
         {
-			SetActualPositon(false);
+            if (Input.IsActionJustPressed("testTop"))
+            {
+                SetActualPositon(true);
+            }
+
+            if (Input.IsActionJustPressed("testBottom"))
+            {
+                SetActualPositon(false);
+            }
+
+            // Pohyb mysi
+            leverGrab.RotateX(Mathf.DegToRad(motionMouse.y * 0.2f));
+
+            // Vypocet pro detekci horniho konecneho bodu a spodniho konecneho bodu
+            float actual_rot = Mathf.RadToDeg(leverGrab.Rotation.x);
+
+            if (actual_rot >= max-1.0f)
+                DetectReachEndPoint(false);
+            else if (actual_rot <= min+1.0f)
+                DetectReachEndPoint(true);
+            else
+            {
+                // normalni chod
+                leverGrab.Freeze = false;
+                leverGrab.Sleeping = false;
+                isReach = false;
+            }
+        }
+        else
+        {
+            if(!isReach)
+            {
+                // Vypocet pro detekci horniho konecneho bodu a spodniho konecneho bodu
+                float actual_rot = Mathf.RadToDeg(leverGrab.Rotation.x);
+
+                if (actual_rot >= max - 1.0f)
+                    DetectReachEndPoint(false);
+                else if (actual_rot <= min + 1.0f)
+                    DetectReachEndPoint(true);
+            }
         }
 
+        // Reset for zero
+        motionMouse = Vector2.Zero;
+    }
+
+    public void DetectReachEndPoint(bool newTop)
+    {
+        SetReachPositionFreeze(newTop);
+        isReach = true;
+    }
+
+    public void SetActualPositon(bool newTop)
+    {
+        if (newTop)
+        {
+            Vector3 oldRot = leverGrab.Rotation;
+            oldRot.x = Mathf.DegToRad(min);
+            leverGrab.Rotation = oldRot;
+        }
+        else
+        {
+            Vector3 oldRot = leverGrab.Rotation;
+            oldRot.x = Mathf.DegToRad(max);
+            leverGrab.Rotation = oldRot;
+        }
+    }
+
+    public void SetReachPositionFreeze(bool newTop)
+    {
+        if (newTop)
+        {
+            Vector3 oldRot = leverGrab.Rotation;
+            oldRot.x = Mathf.DegToRad(min);
+            leverGrab.Rotation = oldRot;
+        }
+        else
+        {
+            Vector3 oldRot = leverGrab.Rotation;
+            oldRot.x = Mathf.DegToRad(max);
+            leverGrab.Rotation = oldRot;
+        }
+
+        leverGrab.CanSleep = true;
+        leverGrab.Sleeping = true;
+        leverGrab.Freeze = true;
+    }
+
+    public void SetMotorToPosition(bool newTop)
+    {
+        if (newTop)
+        {
+            hingeJoint3D.SetParam(HingeJoint3D.Param.MotorTargetVelocity, 1.0f * MotorPower);
+            hingeJoint3D.SetParam(HingeJoint3D.Param.MotorMaxImpulse, 1.0f);
+            hingeJoint3D.SetFlag(HingeJoint3D.Flag.EnableMotor, true);
+        }
+        else
+        {
+            hingeJoint3D.SetParam(HingeJoint3D.Param.MotorTargetVelocity, -1.0f * MotorPower);
+            hingeJoint3D.SetParam(HingeJoint3D.Param.MotorMaxImpulse, 1.0f);
+            hingeJoint3D.SetFlag(HingeJoint3D.Flag.EnableMotor, true);
+        }
+    }
+
+    public void SetMotorDisable()
+    {
+        hingeJoint3D.SetFlag(HingeJoint3D.Flag.EnableMotor, false);
+    }
+
+    public void ActionStart()
+    {
+
+    }
+
+    public void ActionEnd()
+    {
         // Vypocet pro detekci horniho konecneho bodu a spodniho konecneho bodu
         float actual_rot = Mathf.RadToDeg(leverGrab.Rotation.x);
 
-        if (actual_rot >= 44.0f)
-            DetectReachEndPoint(false);
-        else if (actual_rot <= -44.0f)
-            DetectReachEndPoint(true);
+        //Spustime motor tim smerem podle toho kde se aktualne nachazi paka
+
+        if (actual_rot >= 0.0f)
+            SetMotorToPosition(true);
+        else if (actual_rot <= -0.0001f)
+            SetMotorToPosition(false);
     }
-
-	public void DetectReachEndPoint(bool newTop)
-	{
-        SetReachPositionFreeze(newTop);
-
-        if (newTop)
-		{
-			GD.Print("Lever is in Top End positon !");
-		}
-		else
-		{
-            GD.Print("Lever is in Bottom End positon !");
-        }
-	}
-
-	public void SetActualPositon(bool newTop)
-	{
-		if(newTop)
-		{
-            Vector3 oldRot = leverGrab.Rotation;
-			oldRot.x = Mathf.DegToRad(-45.0f);
-			leverGrab.Rotation = oldRot;
-        }
-		else
-		{
-            Vector3 oldRot = leverGrab.Rotation;
-            oldRot.x = Mathf.DegToRad(45.0f);
-            leverGrab.Rotation = oldRot;
-        }
-	}
-
-	public void SetReachPositionFreeze(bool newTop)
-	{
-		if(newTop)
-		{
-            Vector3 oldRot = leverGrab.Rotation;
-            oldRot.x = Mathf.DegToRad(-45.0f);
-            leverGrab.Rotation = oldRot;
-        }
-		else
-		{
-            Vector3 oldRot = leverGrab.Rotation;
-            oldRot.x = Mathf.DegToRad(45.0f);
-            leverGrab.Rotation = oldRot;
-        }
-
-		leverGrab.CanSleep = true;
-		leverGrab.Sleeping = true;
-	}
 
     public virtual void message_update()
     {
@@ -116,12 +179,14 @@ public partial class wall_lever_test : Node3D
 				{
                     interactCharacter = (FPSCharacter_Interaction)interactiveObject.msgObject.GetNodeData();
                     isActionUpdate = true;
+                    ActionStart();
                     break;
 				}
 			case "msg_grab_action_end":
 				{
                     interactCharacter = (FPSCharacter_Interaction)interactiveObject.msgObject.GetNodeData();
 					isActionUpdate = false;
+                    ActionEnd();
                     break;
 				}
             default: break;
