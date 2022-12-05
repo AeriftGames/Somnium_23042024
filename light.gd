a67a2436c82f068b3a23de519618d499286ea6e4
Oalar <tomas.vresky@gmail.com>
2022-12-05 20:13:59 +0100

    x

--- light.gd
+++ light.gd
@@ -1,13 +1,33 @@
 extends Node3D
 
+@export var always_on: bool
+@export var flashing: bool
 
-@onready var material = $Light
+@onready var light = $Light
+@onready var material = light.get_active_material(0)
+@onready var light_on = load("res://light_on.tres")
+@onready var light_off = load("res://light_off.tres")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print(light)
 	print(material)
-	pass # Replace with function body.
+	#material.emission_enabled = self.emission_enabled
+	if always_on:
+		_switch_on()
+	else:
+		_switch_off()
+	if flashing:
+		pass
 
+
+func _switch_on():
+	light.set_surface_override_material(0, light_on)
+
+
+func _switch_off():
+	light.set_surface_override_material(0, light_off)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
