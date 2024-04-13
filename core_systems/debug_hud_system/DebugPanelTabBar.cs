45da057ce6041bd317a2c635ccbe82603f0f16fd
kaencz <kaencz@seznam.cz>
2024-04-13 14:26:23 +0200

    extern update to self reference get CPanelBase in DebugPanelTabBar

--- core_systems/debug_hud_system/DebugPanelTabBar.cs
+++ core_systems/debug_hud_system/DebugPanelTabBar.cs
@@ -3,11 +3,16 @@ using System;
 
 public partial class DebugPanelTabBar : TabBar
 {
-    [Export] public CPanelBase ourPanel;
-
     public void _on_focus_entered()
     {
-        if (ourPanel != null)
-            ourPanel.FocusFirstElement();
+        // we try get node about two levels in and try get as CPanelBase
+        // if success, we focus first element in panel
+        MarginContainer ourMargin = GetChild<MarginContainer>(0);
+        if (ourMargin != null ) 
+        {
+            CPanelBase ourPanel = ourMargin.GetChild<CPanelBase>(0);
+            if (ourPanel != null)
+                ourPanel.FocusFirstElement();
+        }
     }
 }
